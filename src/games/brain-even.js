54add17e31d93ad8555c7d23893f66259fe89535
witcher3025 <a.avsetov@outlook.com>
2023-04-26 14:05:34 +0300

    add src/games/brain-calc.js and brain-even.js

---
+++ src/games/brain-even.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console, import/extensions */
+
+import { greetings, brainEvenCircle } from '../index.js';
+
+const brainEven = () => {
+  console.log('brain-even\n');
+  greetings();
+
+  const showRulesOfGame = () => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  };
+
+  showRulesOfGame();
+
+  const numbersGenerator = () => Math.ceil(Math.random() * 100);
+  const isEven = (number) => {
+    const result = number % 2 === 0 ? 'yes' : 'no';
+    return result;
+  };
+
+  // Передаем в качестве параметров две функции: генерация числа и вычисление правильного ответа
+  // без их вызова. Сам вызов функций будет происходить уже внутри
+  brainEvenCircle(numbersGenerator, isEven);
+};
+
+export default brainEven;
