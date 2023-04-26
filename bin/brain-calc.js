66b6761211468b03f1ea7c883e80d3e898506b01
witcher3025 <a.avsetov@outlook.com>
2023-04-26 14:06:43 +0300

    fix makefile and bin/brain-calc.js,brain-even.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -2,55 +2,6 @@
 
 /* eslint-disable no-console, import/extensions */
 
-import { greetings, brainEvenCircle } from '../src/index.js';
+import brainCalc from '../src/games/brain-calc.js';
 
-console.log('brain-calc\n');
-greetings();
-
-const showRulesOfGame = () => {
-  console.log('What is the result of the expression?');
-};
-
-showRulesOfGame();
-
-const numbersGenerator = () => Math.ceil(Math.random() * 100);
-const operatorGenerator = () => {
-  const operators = ['+', '-', '*'];
-  const index = Math.floor(Math.random() * 3);
-  return operators[index];
-};
-
-const getCreateExpression = () => {
-  const number1 = numbersGenerator();
-  const number2 = numbersGenerator();
-  const operator = operatorGenerator();
-  const expression = `${number1} ${operator} ${number2}`;
-  return expression;
-};
-
-const getCalculateExpression = (expression) => {
-  const [numAsStr1, operator, numAsStr2] = expression.split(' ');
-  const number1 = Number(numAsStr1);
-  const number2 = Number(numAsStr2);
-
-  let result = 0;
-  switch (operator) {
-    case '+':
-      result = number1 + number2;
-      break;
-    case '-':
-      result = number1 - number2;
-      break;
-    case '*':
-      result = number1 * number2;
-      break;
-    default:
-      break;
-  }
-
-  return `${result}`;
-};
-
-// Передаем в качестве параметров две функции: генерация выражения и вычисление
-// правильного ответа без их вызова. Сам вызов функций будет происходить уже внутри
-brainEvenCircle(getCreateExpression, getCalculateExpression);
+brainCalc();
--- makefile
+++ makefile
@@ -5,7 +5,7 @@ brain-games:
 	node bin/brain-games.js
 
 brain-even:
-	node bin/brain-even.js
+	node bin/games/brain-even.js
 
 brain-calc:
 	node bin/brain-calc.js
