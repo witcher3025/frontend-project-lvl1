54b4d2231e0f514b88bc538c037aec999901de45
witcher3025 <a.avsetov@outlook.com>
2023-04-27 21:08:14 +0300

    fix src/games/brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,22 +4,20 @@
 
 import { greetings, brainGames } from '../index.js';
 
+const showRulesOfGame = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+};
+
+const numbersGenerator = () => Math.ceil(Math.random() * 100);
+const isEven = (number) => {
+  const result = number % 2 === 0 ? 'yes' : 'no';
+  return result;
+};
+
 const brainEven = () => {
   console.log('brain-even\n');
   greetings();
-
-  const showRulesOfGame = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  };
-
   showRulesOfGame();
-
-  const numbersGenerator = () => Math.ceil(Math.random() * 100);
-  const isEven = (number) => {
-    const result = number % 2 === 0 ? 'yes' : 'no';
-    return result;
-  };
-
   // Передаем в качестве параметров две функции: генерация числа и вычисление правильного ответа
   // без их вызова. Сам вызов функций будет происходить уже внутри
   brainGames(numbersGenerator, isEven);
