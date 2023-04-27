20b8a0260f4f2ec69c1c44df7612b37aecdf0958
witcher3025 <a.avsetov@outlook.com>
2023-04-27 21:09:59 +0300

    fix src/games/brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,31 +4,29 @@
 
 import { greetings, brainGames } from '../index.js';
 
+const showRulesOfGame = () => {
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+};
+
+const numbersGenerator = () => Math.ceil(Math.random() * 100);
+const isPrime = (number) => {
+  if (number < 2) {
+    return 'no';
+  }
+
+  for (let divider = 2; divider <= number / 2; divider += 1) {
+    if (number % divider === 0) {
+      return 'no';
+    }
+  }
+
+  return 'yes';
+};
+
 const brainPrime = () => {
   console.log('brain-prime\n');
   greetings();
-
-  const showRulesOfGame = () => {
-    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  };
-
   showRulesOfGame();
-
-  const numbersGenerator = () => Math.ceil(Math.random() * 100);
-  const isPrime = (number) => {
-    if (number < 2) {
-      return 'no';
-    }
-
-    for (let divider = 2; divider <= number / 2; divider += 1) {
-      if (number % divider === 0) {
-        return 'no';
-      }
-    }
-
-    return 'yes';
-  };
-
   // Передаем в качестве параметров две функции: генерация числа и вычисление правильного ответа
   // без их вызова. Сам вызов функций будет происходить уже внутри
   brainGames(numbersGenerator, isPrime);
