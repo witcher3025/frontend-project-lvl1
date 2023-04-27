910f0bb3bb929d4c14d62055bcf77c80d04a8559
witcher3025 <a.avsetov@outlook.com>
2023-04-27 21:09:40 +0300

    fix src/games/brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,43 +4,41 @@
 
 import { greetings, brainGames } from '../index.js';
 
+const showRulesOfGame = () => {
+  console.log('Find the greatest common divisor of given numbers.');
+};
+
+const numbersGenerator = () => Math.ceil(Math.random() * 100);
+
+const getCreateExpression = () => {
+  const number1 = numbersGenerator();
+  const number2 = numbersGenerator();
+  const expression = `${number1} ${number2}`;
+  return expression;
+};
+
+const getCalculateDivider = (expression) => {
+  const [numAsStr1, numAsStr2] = expression.split(' ');
+  const number1 = Number(numAsStr1);
+  const number2 = Number(numAsStr2);
+
+  const minNumber = Math.min(number1, number2);
+  const maxNumber = Math.max(number1, number2);
+
+  let divider = 1;
+  for (let i = 1; i <= minNumber; i += 1) {
+    if ((minNumber % i === 0) && (maxNumber % i === 0)) {
+      divider = i;
+    }
+  }
+
+  return `${divider}`;
+};
+
 const brainGcd = () => {
   console.log('brain-gcd\n');
   greetings();
-
-  const showRulesOfGame = () => {
-    console.log('Find the greatest common divisor of given numbers.');
-  };
-
   showRulesOfGame();
-
-  const numbersGenerator = () => Math.ceil(Math.random() * 100);
-
-  const getCreateExpression = () => {
-    const number1 = numbersGenerator();
-    const number2 = numbersGenerator();
-    const expression = `${number1} ${number2}`;
-    return expression;
-  };
-
-  const getCalculateDivider = (expression) => {
-    const [numAsStr1, numAsStr2] = expression.split(' ');
-    const number1 = Number(numAsStr1);
-    const number2 = Number(numAsStr2);
-
-    const minNumber = Math.min(number1, number2);
-    const maxNumber = Math.max(number1, number2);
-
-    let divider = 1;
-    for (let i = 1; i <= minNumber; i += 1) {
-      if ((minNumber % i === 0) && (maxNumber % i === 0)) {
-        divider = i;
-      }
-    }
-
-    return `${divider}`;
-  };
-
   // Передаем в качестве параметров две функции: генерация выражения и вычисление
   // правильного ответа без их вызова. Сам вызов функций будет происходить уже внутри
   brainGames(getCreateExpression, getCalculateDivider);
